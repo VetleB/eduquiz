884a987183dec56f7c16f20c9740ea235d66cba4
ecuatox <a.tenstad@gmail.com>
2017-02-14 14:15:01 +0100

    Create views and templates

--- eduquiz/urls.py
+++ eduquiz/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
+import eduquiz.views as views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]

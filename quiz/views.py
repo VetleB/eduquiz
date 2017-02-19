4742a6cbb15350d8cb121730ce4746cdead9cf38
ecuatox <a.tenstad@gmail.com>
2017-02-19 18:01:24 +0100

    Create multiple choice question view

--- quiz/views.py
+++ quiz/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from quiz.models import MultipleChoiceQuestion
+from quiz.models import *
 
 
 def question(request):
 
+    question = MultipleChoiceQuestion.objects.all()[0]
+    qts = QuestionTopic.objects.filter(question=question)
+    topics = [qt.topic for qt in qts]
+    answers = MultipleChoiceAnswer.objects.filter(question=question)
+
     context = {
-        question: MultipleChoiceQuestion.objects.order_by('?').first(),
+        'question': question,
+        'topics': topics,
+        'answers': answers,
     }
 
-    return render(request, 'quiz/question.html', context)
+    return render(request, 'quiz/multipleChoiceQuestion.html', context)

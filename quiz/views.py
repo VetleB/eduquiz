16de2fc89f130a81e94aa681a1327f38f108097f
ecuatox <a.tenstad@gmail.com>
2017-02-19 19:16:03 +0100

    Create MPQ and TFQ layout
    MultipleChoiceQuestion
    TrueFalseQuestion

--- quiz/views.py
+++ quiz/views.py
@@ -3,8 +3,16 @@ from quiz.models import *
 
 
 def question(request):
+    return trueFalseQuestion(request, None)
+    #return multipleChoiceQuestion(request, None)
 
+
+def multipleChoiceQuestion(request, question):
+
+    ## REMOVE LATER
     question = MultipleChoiceQuestion.objects.all()[0]
+    # END
+
     qts = QuestionTopic.objects.filter(question=question)
     topics = [qt.topic for qt in qts]
     answers = MultipleChoiceAnswer.objects.filter(question=question)
@@ -16,3 +24,22 @@ def question(request):
     }
 
     return render(request, 'quiz/multipleChoiceQuestion.html', context)
+
+
+def trueFalseQuestion(request, question):
+
+    # REMOVE LATER
+    question = TrueFalseQuestion.objects.all()[0]
+    # END
+
+    qts = QuestionTopic.objects.filter(question=question)
+    topics = [qt.topic for qt in qts]
+    answers = ('True', 'False')
+
+    context = {
+        'question': question,
+        'topics': topics,
+        'answers': answers,
+    }
+
+    return render(request, 'quiz/trueFalseQuestion.html', context)

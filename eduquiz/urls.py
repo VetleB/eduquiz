9890703e369f5413ca0ed6fc190e96c40f286c19
Unknown <ludvig_pedersen@hotmail.com>
2017-02-19 19:10:18 +0100

    Create simple login form

--- eduquiz/urls.py
+++ eduquiz/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 import eduquiz.views as views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^authentication/', include('authentication.urls')),
 ]

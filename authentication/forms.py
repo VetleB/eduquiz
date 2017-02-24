458ecedb4d401d5c8caab3b6dae60789110f62dc
ecuatox <a.tenstad@gmail.com>
2017-02-24 12:29:26 +0100

    Add login backend

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,22 @@
 from django import forms
+from django.contrib.auth import authenticate, login
+from django.core.exceptions import ValidationError
 
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=15, label="username")
+    password = forms.CharField(max_length=30, widget=forms.PasswordInput, label="password")
 
-class loginForm(forms.Form):
-    username=forms.CharField(max_length=15, label="username")
-    password=forms.CharField(widget=forms.PasswordInput)
+    def clean(self):
+        form_data = self.cleaned_data
+
+        try:
+            user = authenticate(
+                username = form_data['username'],
+                password = form_data['password']
+            )
+            if user is None:
+                raise ValidationError({'password': 'Wrong username or password.'}, code='invalid')
+        except KeyError:
+            pass
+
+        return form_data
--- authentication/views.py
+++ authentication/views.py
@@ -1,15 +1,28 @@
-
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from authentication.forms import *
-
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as djangologin
+from authentication.forms import LoginForm
+from django.core.exceptions import ValidationError
 
 def login(request):
-    context = {
-    }
-    if request.method=='POST':
-        form = loginForm(request.POST)
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password'],
+            )
+            if user is not None:
+                djangologin(request, user)
+                return HttpResponseRedirect('/')
     else:
-        print("nope")
+        form = LoginForm()
+
+    context = {
+        'form': form,
+    }
 
     return render(request, 'authentication/login.html', context)
-

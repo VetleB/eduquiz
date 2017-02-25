6a6acf6ee5bc25bf4433c437a60cd8c7fa6d5694
vetleB <vetleb@stud.ntnu.no>
2017-02-25 11:12:46 +0100

    Modify validate to account for answers with any number of decimals

--- quiz/tests.py
+++ quiz/tests.py
@@ -4,9 +4,21 @@ from django.contrib.auth.admin import User
 import random
 
 class TextQuestionTestCase(TestCase):
+
+    def setUp(self):
+        player = Player.objects.create(
+            user = User.objects.create(),
+        )
+        question = TextQuestion.objects.create(
+            question_text = 'TEST_QUESTION',
+            creator = player,
+            difficulty = 0.0,
+            answer = '1.000',
+        )
+
     def test_validation_of_correct_answer(self):
-        question = TextQuestion()
-        question.answer = '1.450'
+        question = TextQuestion.objects.get()
+        question.answer = '1.45'
         self.assertTrue(question.validate('1.45'))
         self.assertTrue(question.validate('1.450'))
         self.assertFalse(question.validate('1.460'))
@@ -14,39 +26,41 @@ class TextQuestionTestCase(TestCase):
         self.assertFalse(question.validate('11.455'))
 
     def test_validation_of_comma_as_decimal_mark(self):
-        question = TextQuestion()
-        question.answer = '1.000'
+        question = TextQuestion.objects.get()
         self.assertTrue(question.validate('1.000'))
         self.assertTrue(question.validate('1,000'))
         self.assertTrue(question.validate('1,'))
 
+    def test_validation_of_superfluous_spaces(self):
+        question = TextQuestion.objects.get()
+        self.assertTrue(question.validate(' 1.000'))
+        self.assertTrue(question.validate('1.000 '))
+        self.assertTrue(question.validate(' 1.000 '))
+
     def test_validation_of_answer_without_decimal_part(self):
-        question = TextQuestion()
-        question.answer = '1.000'
+        question = TextQuestion.objects.get()
         self.assertTrue(question.validate('1'))
         self.assertTrue(question.validate('1.'))
 
     def test_validation_of_answer_without_integer_part(self):
-        question = TextQuestion()
+        question = TextQuestion.objects.get()
         question.answer = '0.001'
         self.assertTrue(question.validate('.001'))
         self.assertFalse(question.validate('001'))
 
     def test_validation_of_too_many_decimals(self):
-        question = TextQuestion()
-        question.answer = '1.000'
+        question = TextQuestion.objects.get()
         self.assertFalse(question.validate('1.0001'))
         self.assertTrue(question.validate('1.0000'))
 
     def test_validation_of_too_few_decimals(self):
-        question = TextQuestion()
-        question.answer = '1.000'
+        question = TextQuestion.objects.get()
         self.assertTrue(question.validate('1.'))
         self.assertTrue(question.validate('1.0'))
         self.assertTrue(question.validate('1.00'))
 
     def test_validation_of_zero(self):
-        question = TextQuestion()
+        question = TextQuestion.objects.get()
         question.answer = '0.000'
         self.assertTrue(question.validate('0'))
         self.assertTrue(question.validate('0.'))
@@ -55,14 +69,14 @@ class TextQuestionTestCase(TestCase):
         self.assertTrue(question.validate('0.00000000000'))
 
     def test_validation_of_leading_zeroes(self):
-        question = TextQuestion()
+        question = TextQuestion.objects.get()
         question.answer = '10.000'
         self.assertTrue(question.validate('010'))
         self.assertTrue(question.validate('00000000000010'))
         self.assertTrue(question.validate('010.000'))
 
     def test_validation_of_hexadecimal_capitalization(self):
-        question = TextQuestion()
+        question = TextQuestion.objects.get()
         question.answer = 'aB3.bF1'
         self.assertTrue(question.validate('ab3.bf1'))
         self.assertTrue(question.validate('AB3.BF1'))

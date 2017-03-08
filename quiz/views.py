8a7f6a055b3042863b167c3f25bb18406bff0d80
ecuatox <a.tenstad@gmail.com>
2017-03-08 14:38:23 +0100

    Fix truefalse question

--- quiz/views.py
+++ quiz/views.py
@@ -105,7 +105,7 @@ def multipleChoiceQuestion(request, question):
 
 def trueFalseQuestion(request, question):
 
-    answers = ('True', 'False')
+    answers = (('true', 'True'), ('false', 'False'))
 
     context = {
         'question': question,
